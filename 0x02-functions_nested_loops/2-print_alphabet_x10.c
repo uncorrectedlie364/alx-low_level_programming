cadf86244a2395de4b0a968a29681cd572fb6813
root <mcclarerodgy@gmail.com>
2023-03-28 02:42:14 -0700

    2. 10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,11 @@
 #include "main.h"
 
-	/**
-	 * main - check the code
-	 *
-	 * Return: Always 0.
-	 */
-	
+/**
+* main - check the code
+* Return: Always 0.
+*/
 	int main(void)
 {
 	print_alphabet();
-	return(0);
+	return (0);
 }
