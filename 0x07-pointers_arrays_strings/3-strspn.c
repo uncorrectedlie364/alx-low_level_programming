207ec994b73b28688466b4aa31e7ee525fd509a3
root <mcclarerodgy@gmail.com>
2023-05-03 16:13:18 -0700

    strspn update

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,32 +1,30 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
- * _strspn - Entry point
- * @s: input
- * @accept: input
- * Return: Always 0 (Success)
+ * _strspn - length of prefix substring
+ * @s: string to go through
+ * @accept: accepted bytes
+ * Return: returns usigned values
  */
-
 unsigned int _strspn(char *s, char *accept)
-
 {
-	unsigned int n = 0;
-	int r;
+	int i;
+	int j;
+	unsigned int result = 0;
 
-	while (*s)
-
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (r = 0; accept[r]; r++)
+		for (j = 0; s[j] != '\0'; j++)
 		{
-			if (*s == accept[r])
-					{
-					n++;
-					break;
-					}
-					else if (accept[r + 1] == '\0')
-					return (n);
-					}
-					s++;
-					}
-					return (n);
-					}
+			if (s[i] == accept[j])
+			{
+				result++;
+				break;
+			}
+		}
+
+	if (s[j] == '\0')
+		return (result);
+	}
+	return (result);
+}
