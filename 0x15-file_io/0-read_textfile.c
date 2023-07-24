15cb73945f66bc1699d93aa8bb8e96f7925c7de9
uncorrectedlie364 <mcclarerodgy@gmail.com>
2023-07-24 03:18:04 -0700

    updating description for function read_textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,25 +1,36 @@
 #include "main.h"
 #include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
 
-/*
- * read_textfile : read text file print to STDOUT.
- * @filename : text file being read
- * @letters : number of letters to be read
- * Return : w- actual number of bytes read and printed
+/**
+ * read_textfile - Read text file print to STDOUT.
+ * @filename: The text file being read.
+ * @letters: Number of letters to be read.
+ *
+ * Return: The actual number of bytes read and printed.
  * 0 when function fails or filename is NULL.
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *buf;
-	ssize_t fd;
-	ssize_t w;
-	ssize_t t;
+	ssize_t fd, w, t;
 
-	fd = open(filename, O_RDONLY);
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O RDONLY);
 	if (fd == -1)
 		return (0);
+
 	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+
 	t = read(fd, buf, letters);
 	w = write(STDOUT_FILENO, buf, t);
 
@@ -27,4 +38,3 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	close(fd);
 	return (w);
 }
-
