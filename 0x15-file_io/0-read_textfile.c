b2f211f3f6411b4f2616031905c4cab4ffe1519a
uncorrectedlie364 <mcclarerodgy@gmail.com>
2023-07-24 03:26:21 -0700

    updated descrioption for read_textfile function

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -20,7 +20,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 
-	fd = open(filename, O RDONLY);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 
